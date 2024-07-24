a1a6f0c759b3308e82a0554063d4fda2df3a0bf5
Prakash Karuppusamy <35074120+prakashbtech87@users.noreply.github.com>
2024-07-24 12:53:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/partials/NavBar';
+import React from 'react';
+
+function App() {
+  return (
+   <React.Fragment>
+     <NavBar/>
+     </React.Fragment>
+  );
+}
+
+export default App;
