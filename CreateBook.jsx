026d5555500860fc10301f3034b222090ff761a9
Prakash Karuppusamy <35074120+prakashbtech87@users.noreply.github.com>
2024-07-26 10:11:32 +0530

    Add files via upload

--- CreateBook.jsx
+++ CreateBook.jsx
@@ -1,31 +1,54 @@
+import { useState } from "react"
 
 
 export default function CreateBook() {
+    const [book, setBook] = useState({
+        title: "",
+        author: "Unknown",
+        pageCount: 0,
+        year: 2024,
+        language: ""
+    });
+
+
+    const changeHandler = (e) => {
+        console.log(e.target.name, e.target.value);
+        setBook({
+            ...book,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const submitHandler = (e) => {
+        console.log(book);
+
+    }
+
     return (
         <div className="container">
             <div className="row">
                 <div className="col-sm-12 col-md-6 col-lg-4 offset-md-3 offset-lg-4">
                     <h1>Add New Book</h1>
-                    <form>
+                    <form onSubmit={submitHandler}>
                         <div className="mb-3">
                             <label htmlFor="title" className="form-label">Title</label>
-                            <input type="text" className="form-control" id="title" />
+                            <input type="text" value={book.title} onChange={changeHandler} className="form-control" id="title" name="title" />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="author" className="form-label">Author</label>
-                            <input type="text" className="form-control" id="author" />
+                            <input type="text" value={book.author} onChange={changeHandler} className="form-control" id="author" name="author" />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="pageCount" className="form-label">Page Count</label>
-                            <input type="number" className="form-control" id="pageCount" />
+                            <input type="number" value={book.pageCount} onChange={changeHandler} className="form-control" id="pageCount" name="pageCount" />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="year" className="form-label">Year</label>
-                            <input type="number" className="form-control" id="year" />
+                            <input type="number" value={book.year} onChange={changeHandler} className="form-control" id="year" name="year" />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="language" className="form-label">Language</label>
-                            <input type="text" className="form-control" id="language" />
+                            <input type="text" value={book.language} onChange={changeHandler} className="form-control" id="language" name="language" />
                         </div>
                         <button type="submit" className="btn btn-primary">Create</button>
                     </form>
